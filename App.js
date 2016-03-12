841e161ef6e1d05fa405452eee2def7ece80ec27
Dmitriy Lysenko <Dmitriy Lysenko>
2016-03-12 17:23:26 +0200

    Добавлен VKController, обвертка над VKSDK

--- App.js
+++ App.js
@@ -1,45 +1,31 @@
 
 
 var remote = require('remote');
-var VK = require('vksdk');
 var querystring = require('querystring');
 var level = require('level');
-var BrowserWindow = remote.require('browser-window')
+var BrowserWindow = remote.require('browser-window');
+var VKController = require('./src/VKController');
 var currentWindow = remote.getCurrentWindow();
 
+function App(){
 
-function App(window){
-
-	this.window = window;
-
+	this.vk = new VKController();
 	this.db = level('./db');
 
-	this.vkData = {
-				id: 5329877,
-				secret: 'SNY83LI0pH56lWZqB3R5',
-				access_token: null,
-				expires_in: null,
-				user_id: null 
-			};
-
-	this.vk = new VK({
-		'appId'     : this.vkData.id,
-		'appSecret' : this.vkData.secret,
-		'mode'      : 'oauth'
-	});
+	this.vk.on('onInit', this.load.bind(this));
+	this.vk.on('onLoadedFiends', this.initViewVKFriends.bind(this));
+	this.vk.on('onLogin', this.initViewVKFriends.bind(this));
 
 	this.init();
 
 }
-
-App.prototype.vkData = {};
 
 App.prototype.init = function(callback){
 	this.db.get('vkData', {valueEncoding: 'json'}, function(e, data){
 
 		if(e) {
 			if(e.notFound) {
-				this.authorizationToVk();
+				this.vk.login();
 				return;
 			}
 
@@ -47,73 +33,48 @@ App.prototype.init = function(callback){
 			return;
 		}
 
-		this.setVkUserData(data);
-		this.intiVk();
+		this.vk.init(data);
+
 	}.bind(this));
 };
 
-App.prototype.setVkUserData = function(data){
-	this.vkData.access_token = data.access_token;
-	this.vkData.expires_in = data.expires_in;
-	this.vkData.user_id = data.user_id;
+App.prototype.load = function() {
+	this.vk.loadFriends();
 };
 
-App.prototype.authorizationToVk = function(){
+App.prototype.saveVkData = function(data){
 
-	var authWindow = new BrowserWindow({ center: true, autoHideMenuBar: true, maximizable: false, resizable: false }),
-		webContents = authWindow.webContents,
-		_this = this;
+	var dbOptions = [{
+		type          : 'put',
+		key           : 'config',
+		value         : data,
+		keyEncoding   : 'binary',
+		valueEncoding : 'json'
+	}];
 
-	authWindow.loadURL('https://oauth.vk.com/authorize?client_id=5329877&display=popup&redirect_uri=https://oauth.vk.com/blank.html&scope=messages&response_type=token&v=5.45');
-
-	webContents.on('did-get-redirect-request', function(e, oldURL, newURL, a, v, d){
-
-		if(newURL.search('https://oauth.vk.com/blank.html') != -1){
-			authWindow.close();
-			var data = querystring.parse(newURL.substring(newURL.indexOf('#') + 1));
-			var dbOptions = [{
-				type          : 'put',
-				key           : 'vkData',
-				value         : data,
-				keyEncoding   : 'binary',
-				valueEncoding : 'json'
-			}];
-
-			_this.db.batch(dbOptions, function(e){
-
-				if(e){
-					console.log(e.message);
-					return;
-				}
-
-				_this.setVkUserData(data);
-				_this.intiVk();
-			});
+	this.db.batch(dbOptions, function(e){
+		if(e){
+			console.log(e.message);
+			return;
 		}
-
 	});
 
 };
 
-App.prototype.intiVk = function(){
 
-	this.vk.setSecureRequests(true);
-	this.vk.on('tokenByCodeReady', function() { });
-	this.vk.on('tokenByCodeNotReady', function(e) {
-		// обрабатываем ошибку установки токена 
-		console.dir(e);
-		console.log('tokenByCodeNotReady');
-	});
-	this.vk.setToken( this.vkData.access_token );
-	this.vk.request('messages.getDialogs', {'uids' : this.vkData.user_id}, function(e) {
-		console.dir(e.response.items);
-		var div = document.createElement('div')
-		document.body.appendChild(div);
-	});
 
-};
+App.prototype.initViewVKFriends = function(friends) {
+	console.dir(friends);
+	var div = null, img = null,
+		container = document.getElementById('friends'),
+		fr;
 
-App.prototype.getAllFriends = function(callback){
+	for(var i = 0; i < friends.length; i += 1){
+		fr = friends[i];
+		div = document.createElement('div');
+		div.innerHTML = '<image src=' + fr.photo_50+ ' style="width:50px; height: 50px;" ></image>' + fr.first_name + ' ' + fr.last_name + ' ' + (fr.online ? 'online' : 'offline');
+		container.appendChild(div);
+	}
 
 };
 
