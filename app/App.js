1d351922c574a2175a31832b716ca7c8733a05c5
Dmitry Lysenko <demansn@gmail.com>
2016-03-24 07:25:12 +0200

    Рефакторинг

--- app/App.js
+++ app/App.js
@@ -1,165 +1,41 @@
+'use strict';
 
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const querystring = require('querystring');
 
-var remote = require('remote');
-var querystring = require('querystring');
-var level = require('level');
-var BrowserWindow = remote.require('browser-window');
-var VKController = require('./src/VKController');
-var currentWindow = remote.getCurrentWindow();
+let mainWindow;
+let dialogWindow;
+let loginWindow;
 
-function App(){
+function onReady () {
+	
+	// Create the browser window.
+	mainWindow = new BrowserWindow({width: 800, height: 600, center: true, autoHideMenuBar: true, maximizable: false, resizable: false});
+	mainWindow.loadURL('file://' + __dirname + '/src/index.html');
 
-	this.vk = new VKController();
-	this.db = level('./db');
+	// Open the DevTools.
+	//mainWindow.webContents.openDevTools();
 
-	this.vk.on('onInit', this.load.bind(this));
-	this.vk.on('onLoadedFiends', this.initViewVKFriends.bind(this));
-	this.vk.on('onLoadedMessages', this.showMessages.bind(this));
-	this.vk.on('onLoadedMesage', this.addMessages.bind(this));
-	this.vk.on('onSendedMesage', this.onSendedMesage.bind(this));
-	this.vk.on('onLogin', this.initViewVKFriends.bind(this));
-
-	$('#friends').on('click', '.friend', this.onClickFriend.bind(this));
-	$('#button-send-message').on('click', this.onClickSendMessage.bind(this));
-
-	this.init();
 
 }
 
-App.prototype.init = function(callback){
-	this.db.get('vkData', {valueEncoding: 'json'}, function(e, data){
 
-		if(e) {
-			if(e.notFound) {
-				this.vk.login();
-				return;
-			}
+app.on('ready', onReady);
 
-			console.log(e.message);
-			return;
-		}
-
-		this.vk.init(data);
-
-	}.bind(this));
-};
-
-App.prototype.load = function() {
-	this.vk.loadFriends();
-};
-
-App.prototype.saveVkData = function(data){
-
-	var dbOptions = [{
-		type          : 'put',
-		key           : 'config',
-		value         : data,
-		keyEncoding   : 'binary',
-		valueEncoding : 'json'
-	}];
-
-	this.db.batch(dbOptions, function(e){
-		if(e){
-			console.log(e.message);
-			return;
-		}
-	});
-
-};
-
-App.prototype.initViewVKFriends = function(friends) {
-
-	var div = null, img = null,
-		container = document.getElementById('friends'),
-		fr;
-
-	for(var i = 0; i < friends.length; i += 1){
-		fr = friends[i];
-		div = document.createElement('div');
-		div.id = fr.id;
-		div.className = 'friend';
-		div.innerHTML = '<image src=' + fr.photo_50+ ' style="width:50px; height: 50px;" ></image>' + fr.first_name + ' ' + fr.last_name + ' ' + (fr.online ? 'online' : 'offline');
-		container.appendChild(div);
+app.on('window-all-closed', function () {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
 	}
+});
 
-};
-
-App.prototype.onClickFriend = function(e) {
-	this.loadDialogByUser(e.currentTarget.id);
-};
-
-App.prototype.loadDialogByUser = function(userId) {
-
-	if(this.currentUserId){
-		$('#' + this.currentUserId).removeClass('selected');
+app.on('activate', function () {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
 	}
-
-	this.currentUserId = userId;
-
-	$('#' + this.currentUserId).addClass('selected');
-
-	this.vk.loadMessagesByUser({'user_id': userId});
-
-};
-
-App.prototype.showMessages = function(messages) {
-	$('#message-list').children().remove();
-
-	var div,
-		m;
-
-	for(var i = messages.length - 1; i >= 0; i -= 1){
-
-		m = messages[i];
-		div = document.createElement('div');
-		div.id = m.id;
-
-		if(m.from_id == this.currentUserId){
-			div.className = 'message friend-message';
-		} else {
-			div.className = 'message owner-message';
-		}
-
-		div.innerHTML = m.body;
-
-		$('#message-list').append(div);
-	}
-
-};
-
-App.prototype.addMessages = function(messages) {
-	var div,
-		m;
-
-	for(var i = messages.length - 1; i >= 0; i -= 1){
-
-		m = messages[i];
-		div = document.createElement('div');
-		div.id = m.id;
-
-		if(m.from_id == this.currentUserId){
-			div.className = 'message friend-message';
-		} else {
-			div.className = 'message owner-message';
-		}
-
-		div.innerHTML = m.body;
-
-		$('#message-list').append(div);
-	}
-
-};
-
-App.prototype.onClickSendMessage = function() {
-	var inp = $('#text-message');
-	if(this.currentUserId) {
-		this.vk.sendMessageByUser({user_id: this.currentUserId, message: inp.val()});
-		inp.val('');
-	}
-};
-
-App.prototype.onSendedMesage = function(responce) {
-	this.vk.loadMessage({message_ids: responce});
-};
-
-module.exports = App;
+});
